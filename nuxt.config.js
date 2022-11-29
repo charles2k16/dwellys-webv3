73412254a8f033e510b9a6cd063b9f3de14cfec4
DeHerro1 <39437603+DeHerro1@users.noreply.github.com>
2022-11-29 19:00:16 +0000

    api change

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,11 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // http://localhost:8000/api/v1
     // baseURL: 'https://newapi.dwellys.com/api',
-    baseURL: process.env.API_URL,
+    baseURL: 'https://api3.dwellys.com/api/v3',
+    // `${process.env.API_URL}/api/v3`,
+    // process.env.NODE_ENV === 'development'
+    //   ? 'http://127.0.0.1:8000/api/v3'
+    //   : 'https://newapi.dwellys.com/api',
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
